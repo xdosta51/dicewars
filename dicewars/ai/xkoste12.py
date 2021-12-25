0d752fd69ca8cd0c10465a479489ee9da0c45be5
Martin Kostelnik <martinkostelnik0@gmail.com>
2021-12-25 11:45:23 +0100

    Add max_n skeleton

--- dicewars/ai/xkoste12.py
+++ dicewars/ai/xkoste12.py
@@ -3,6 +3,10 @@ import random
 
 from dicewars.client.ai_driver import BattleCommand, EndTurnCommand, TransferCommand
 from dicewars.ai.utils import possible_attacks
+from dicewars.client.game.board import Board
+from dicewars.client.game.area import Area
+
+from copy import deepcopy
 
 
 class AI:
@@ -22,6 +26,11 @@ class AI:
 
         self.player_name = player_name
         self.logger = logging.getLogger('AI')
+        self.max_transfers = max_transfers
+        self.players_order = players_order
+        self.board = board
+
+        self.MAXN_MAX_DEPTH = 3
 
     def ai_turn(self, board, nb_moves_this_turn, nb_transfers_this_turn, nb_turns_this_game, time_left):
         """AI agent's turn (a single action)
@@ -35,12 +44,45 @@ class AI:
 
         Returns:    
         BattleCommand() || EndTurnCommand() || TransferCommand()
-        """
-        
-        attacks = list(possible_attacks(board, self.player_name))
-        if attacks:
-            source, target = random.choice(attacks)
-            return BattleCommand(source.get_name(), target.get_name())
-        else:
-            self.logger.debug("No more possible turns.")
-            return EndTurnCommand()
+        """ 
+
+        self.board = board
+        turn, src, tgt = self.get_best_move(board)
+
+        if turn:
+            return BattleCommand(src, tgt) if type(turn) is BattleCommand else TransferCommand(src, tgt)
+
+        return EndTurnCommand()
+
+    def get_best_move(self, board: Board):
+        return self.maxn(board, 10)
+    
+    def maxn(self, board: Board, depth: int):
+        moves = possible_attacks(board, self.player_name) + self.possible_transfers(board)
+
+        if not moves or not depth: # No moves possible
+            return self.evaluate_state(board), None
+
+        for src, tgt in moves:
+            new_board = deepcopy(board)
+            self.simulate_move(new_board, src, tgt)
+
+            evaluation, _ = self.maxn(board, depth - 1)
+
+    def simulate_move(self, board: Board, src, tgt):
+        pass
+
+    def evaluate_state(self, board: Board):
+        return random.sample(range(0, 10), 4)
+
+    def possible_transfers(self, board: Board):
+        for area in self.board.get_player_areas(self.player_name): 
+            if not area.can_attack(): # can_attack means that area has more than one dice => can_attack <=> can_transfer
+                continue
+
+            neighbours = area.get_adjacent_areas_names()
+
+            for adj in neighbours:
+                adjacent_area = self.board.get_area(adj)
+                if adjacent_area.get_owner_name() == self.player_name and adjacent_area.get_dice() != 8:
+                    yield (area, adjacent_area)
